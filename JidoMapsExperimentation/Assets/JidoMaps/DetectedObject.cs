8f5862ce005e9bfb0f0e370a00a2c382de36b6ae
Eashaan Kumar <eashaan@kumar.co>
2018-07-31 23:59:30 -0400

    Created Unity Project

---
+++ JidoMapsExperimentation/Assets/JidoMaps/DetectedObject.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class DetectedObjects {
+	public List<DetectedObject> Objects;
+}
+
+[Serializable]
+public class DetectedObject
+{
+	public string Name;
+	public float X;
+	public float Y;
+	public float Z;
+	public float Confidence;
+	public float Width;
+	public float Height;
+	public float Depth;
+	public float Orientation;
+	public int SeenCount;
+	public int Id;
+
+	public Vector3 Position {
+		get {
+			return new Vector3 (X, Y, -Z);
+		}
+	}
+
+	public DetectedObject (string name, float centerX, float centerY, float centerZ, float confidence, float width, float height, float depth, float orientation, int seenCount, int id)
+	{
+		this.Name = name;
+		this.X = centerX;
+		this.Y = centerY;
+		this.Z = centerZ;
+		this.Confidence = confidence;
+		this.Width = width;
+		this.Height = height;
+		this.Depth = depth;
+		this.Orientation = orientation;
+		this.SeenCount = seenCount;
+		this.Id = id;
+	}
+
+	public override string ToString ()
+	{
+		return string.Format ("{0}, {1}, {2}, {3}", this.Name, this.X, this.Y, this.Z);
+	}
+}
---
+++ JidoMapsExperimentation/Assets/JidoMaps/UnityMapsyncLibNativeInterface.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+namespace UnityEngine.XR.iOS {
+	public class UnityMapsyncLibNativeInterface {
+		[DllImport("__Internal")]
+		private static extern IntPtr _CreateMapsyncSession(IntPtr arSession, string mapId, string userId, string developerKey, float screenHeight, float screenWidth, bool isMappingMode);
+
+		[DllImport("__Internal")]
+		private static extern void _SaveAssets(string assetJson);
+
+		[DllImport("__Internal")]
+		private static extern void _RegisterUnityCallbacks(string callbackGameObject, string assetReloadedCallback, string statusUpdatedCallback, string storePlacementCallback, string progressCallback, string objectDetectedCallback);
+
+        [DllImport("__Internal")]
+        private static extern void _Dispose();
+
+		/// <summary>
+		/// This should only be called once from MapsyncLb.cs
+		/// </summary>
+		public UnityMapsyncLibNativeInterface(string mapId, string userId, string developerKey, bool isMappingMode) {
+			UnityARSessionNativeInterface arkit = UnityARSessionNativeInterface.GetARSessionNativeInterface ();
+			IntPtr arSession = arkit.GetSession ();
+			if (arSession == IntPtr.Zero) 
+			{
+				Debug.Log ("ARKit session is not initialized");
+				return;
+			}
+
+			_CreateMapsyncSession(arSession, mapId, userId, developerKey, (float)Screen.height, (float)Screen.width, isMappingMode);
+
+			string unityCallbackGameObject = "MapSession";
+			string unityAssetLoadedCallbackFunction = "AssetReloaded";
+			string unityStatusUpdatedCallback = "StatusUpdated";
+			string unityStorePlacementCallback = "PlacementStored";
+            string unityProgressCallback = "ProgressIncremented";
+            string unityObjectDetectedCallback = "ObjectDetected";
+            _RegisterUnityCallbacks (unityCallbackGameObject, unityAssetLoadedCallbackFunction, unityStatusUpdatedCallback, unityStorePlacementCallback, unityProgressCallback, unityObjectDetectedCallback);
+		}
+
+        public void Dispose() {
+            _Dispose();
+        }
+
+		public void SaveAssets(List<MapAsset> assets) {
+			MapAssets mapAssets = new MapAssets () {
+				Assets = assets
+			};
+
+			string assetJson = JsonUtility.ToJson (mapAssets);
+
+			Debug.Log ("Asset json: " + assetJson);
+			_SaveAssets(assetJson);
+		}
+	}
+}
